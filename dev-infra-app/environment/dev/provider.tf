fd8b9503a54edd1a7a658a85221182f44dc3193b
Ajay Singh <ajays1268@gmail.com>
2026-04-23 22:52:35 +0530

    Update Ingress and values configuration; add TLS settings and refine ingress class definition

--- dev-infra-app/environment/dev/provider.tf
+++ dev-infra-app/environment/dev/provider.tf
@@ -11,6 +11,7 @@ terraform {
   #   storage_account_name = "kionstgaccount1"
   #   container_name       = "kioncontainer"
   #   key                  = "kion.ttstate"
+  # tennant_id = "72f988bf-86f1-4111-9d98-000d3adfba00"
   # }
 
 }
